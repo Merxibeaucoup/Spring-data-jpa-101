5b820e1eb17dc2169da7937578a2bff1b44f1c95
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-06-11 22:52:01 -0400

    put patch

--- src/main/java/com/edgar/jpa/controllers/ProductController.java
+++ src/main/java/com/edgar/jpa/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.edgar.jpa.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +50,11 @@ public class ProductController {
 		Page<Product> paginatedProducts = productService.findProductsWithPaginationAndSorting(offSet, pageSize, field);
 		return new APIResponse<>(paginatedProducts.getSize(), paginatedProducts);
 
+	}
+	
+	@PatchMapping("/update/field/{id}")
+	public ResponseEntity<Product> updateProductField(@PathVariable Long id, Map<String, Object> fields){
+		return ResponseEntity.ok(productService.updateProductFields(id, fields));
 	}
 
 }
--- src/main/java/com/edgar/jpa/services/ProductService.java
+++ src/main/java/com/edgar/jpa/services/ProductService.java
@@ -1,6 +1,9 @@
 package com.edgar.jpa.services;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -9,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
 import com.edgar.jpa.models.Product;
 import com.edgar.jpa.repositories.ProductRepository;
@@ -23,10 +27,10 @@ public class ProductService {
 	private final ProductRepository productRepository;
 	
 	
-	//initialize data in DB -> 50 count
+	//initialize data in DB -> 10 count products 
 	@PostConstruct
 	public void initDB() {
-		List<Product> products = IntStream.rangeClosed(1,50)
+		List<Product> products = IntStream.rangeClosed(1,10)
 				.mapToObj(i -> new Product("Product"+i, new Random().nextInt(100), new Random().nextLong(50000)))
 				.collect(Collectors.toList());
 		productRepository.saveAll(products);
@@ -51,7 +55,6 @@ public class ProductService {
 	public Page<Product> findProductsWithPagination(int offSet, int pageSize) {
 		Page<Product> products = productRepository.findAll(PageRequest.of(offSet, pageSize));
 		return products;
-
 	}
 	
 	/*
@@ -65,6 +68,22 @@ public class ProductService {
 		
 		return products;
 
+	}
+	
+	public Product updateProductFields(Long id, Map<String, Object> fields) {
+	Optional<Product> product = productRepository.findById(id);
+		
+	if(product.isPresent()) {
+		fields.forEach((key, value)->{
+			Field field = ReflectionUtils.findField(Product.class, key);
+			field.setAccessible(true);
+			ReflectionUtils.setField(field, product.get(), value);
+		});
+	}
+		
+		
+		return productRepository.save(product.get());
+		
 	}
 	
 	
